c058fb0ba14d6162820e78f3474fb58b853c29b9
arpitjangid <arpit@LAPTOP-20282G6Q.wv.cc.cmu.edu>
2019-12-09 18:26:45 -0500

    update data-split with test split

--- data_split.py
+++ data_split.py
@@ -4,49 +4,64 @@ import numpy as np
 import random
 import shutil 
 
-data_path = "../data/FID-300/"
-probe_folder = os.path.join(data_path,"tracks_cropped")
-probe_train = os.path.join(data_path,"tracks_cropped_train")
-probe_val = os.path.join(data_path,"tracks_cropped_val")
+def copy_files(src_dir, dst_dir, ids):
+    for id in ids:
+        src = os.path.join(src_dir,"{:05d}".format(id+1)+".jpg")
+        dst = os.path.join(dst_dir,"{:05d}".format(id+1)+".jpg")
+        shutil.copyfile(src, dst) 
 
-if(not os.path.exists(probe_train)):
-    os.makedirs(probe_train)
+if __name__ == "__main__":
 
-if(not os.path.exists(probe_val)):
-    os.makedirs(probe_val)
+    data_path = "../data/FID-300/"
+    probe_folder = os.path.join(data_path,"tracks_cropped")
+    probe_train = os.path.join(data_path,"tracks_cropped_train")
+    probe_val = os.path.join(data_path,"tracks_cropped_val")
+    probe_test = os.path.join(data_path,"tracks_cropped_test")
 
-flist = os.listdir(probe_folder)
-flist.sort()
+    if(not os.path.exists(probe_train)):
+        os.makedirs(probe_train)
 
-fraction_train = 0.8
-num_files = len(flist)
-num_train = int(fraction_train*num_files)
+    if(not os.path.exists(probe_val)):
+        os.makedirs(probe_val)
 
-label_file = open(data_path+'label_table.csv')
-label_map = np.loadtxt(label_file,delimiter=',',dtype='int')
+    if(not os.path.exists(probe_test)):
+        os.makedirs(probe_test)
 
-random.seed(701)
-train_ids = random.sample(list(np.arange(num_files)),num_train)
-train_ids.sort()
-val_ids = list(set(np.arange(num_files))-set(train_ids))
-val_ids.sort()
+    flist = os.listdir(probe_folder)
+    flist.sort()
 
-label_map_train = label_map[train_ids]
-label_map_val = label_map[val_ids]
+    fraction_train = 0.7
+    fraction_val = 0.1
+    fraction_test = 0.2
+    num_files = len(flist)
+    num_train = int(fraction_train*num_files)
+    num_val = int(fraction_val*num_files)
 
-for id in train_ids:
-    src = os.path.join(probe_folder,"{:05d}".format(id+1)+".jpg")
-    dst = os.path.join(probe_train,"{:05d}".format(id+1)+".jpg")
-    shutil.copyfile(src, dst) 
+    label_file = open(data_path+'label_table.csv')
+    label_map = np.loadtxt(label_file,delimiter=',',dtype='int')
 
-for id in val_ids:
-    src = os.path.join(probe_folder,"{:05d}".format(id+1)+".jpg")
-    dst = os.path.join(probe_val,"{:05d}".format(id+1)+".jpg")
-    shutil.copyfile(src, dst) 
+    random.seed(10701)
+    train_val_ids = random.sample(list(np.arange(num_files)),num_train+num_val)
+    train_ids = train_val_ids[:num_train]
+    train_ids.sort()
+    val_ids = train_val_ids[num_train:]
+    val_ids.sort()
+    test_ids = list(set(np.arange(num_files))-set(train_val_ids))
+    test_ids.sort()
 
-# save csv files
-label_train_path = os.path.join(data_path,"label_table_train.csv")
-label_val_path = os.path.join(data_path,"label_table_val.csv")
+    label_map_train = label_map[train_ids]
+    label_map_val = label_map[val_ids]
+    label_map_test = label_map[test_ids]
 
-np.savetxt(label_train_path,label_map_train,fmt="%d", delimiter=",")
-np.savetxt(label_val_path,label_map_val,fmt="%d", delimiter=",")
+    copy_files(probe_folder, probe_train, train_ids)
+    copy_files(probe_folder, probe_val, val_ids)
+    copy_files(probe_folder, probe_test, test_ids)
+
+    # save csv files
+    label_train_path = os.path.join(data_path,"label_table_train.csv")
+    label_val_path = os.path.join(data_path,"label_table_val.csv")
+    label_test_path = os.path.join(data_path,"label_table_test.csv")
+
+    np.savetxt(label_train_path,label_map_train,fmt="%d", delimiter=",")
+    np.savetxt(label_val_path,label_map_val,fmt="%d", delimiter=",")
+    np.savetxt(label_test_path,label_map_test,fmt="%d", delimiter=",")
